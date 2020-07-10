f680e3f20605e3b36561785f595efb651f1991d4
Tron <crazynuggets@outlook.com>
2020-07-10 21:07:47 +0300

    Updated mysql_library to prevent string injection

--- [Library]/mysql_library/exports/server.lua
+++ [Library]/mysql_library/exports/server.lua
@@ -27,7 +27,8 @@ end
 function doesTableExist(tableName)
 
     if not connectedDB or not tableName then return false end
-    local query = connectedDB:query("SELECT table_name FROM information_schema.tables WHERE `table_schema`='"..(connection.database).."'")
+
+    local query = connectedDB:query("SELECT `table_name` FROM information_schema.tables WHERE `table_schema`=?", connection.database) 
     if not query then return false end
     local result = query:poll(-1)
     if query then
@@ -54,9 +55,10 @@ end
 function doesColumnExist(tableName, columnName)
 
     if not connectedDB or not tableName or not columnName then return false end
+
     local isTable = doesTableExist(tableName)
     if not isTable then return false end
-    local query = connectedDB:query("DESCRIBE `"..tableName.."`")
+    local query = connectedDB:query("DESCRIBE `??`", tableName)
     if not query then return false end
     local result = query:poll(-1)
     if query then
@@ -76,26 +78,26 @@ function doesColumnExist(tableName, columnName)
 end
 
 
---------------------------------------
---[[ Function: Retrieves Row Data ]]--
---------------------------------------
+----------------------------------------
+--[[ Function: Retrieves Row's Data ]]--
+----------------------------------------
 
 function getRowData(tableName, key, keyColumnName, dataColumnName)
 
     if not connectedDB or not tableName or not key or not keyColumnName or not dataColumnName then return false end
+
     local isKeyColumn = doesColumnExist(tableName, keyColumnName)
     if not isKeyColumn then return false end
     local isDataColumn = doesColumnExist(tableName, dataColumnName)
     if not isDataColumn then return false end
-    local query = connectedDB:query("SELECT `"..dataColumnName.."` FROM `"..tableName.."` WHERE `"..keyColumnName.."`='"..key.."'")
+    local query = connectedDB:query("SELECT `??` FROM `??` WHERE `??`=?", dataColumnName, tableName, keyColumnName, key)
     if not query then return false end
     local result = query:poll(-1)
     if query then
         query:free()
     end
     if result and type(result) == "table" and #result > 0 then
-        local value = result[1]
-        return value[dataColumnName]
+        return result[1][dataColumnName]
     else
         return false
     end
@@ -103,22 +105,21 @@ function getRowData(tableName, key, keyColumnName, dataColumnName)
 end
 
 
----------------------------------
---[[ Function: Sets Row Data ]]--
----------------------------------
+-----------------------------------
+--[[ Function: Sets Row's Data ]]--
+-----------------------------------
 
 function setRowData(tableName, key, keyColumnName, dataColumnName, data)
 
     if not connectedDB or not tableName or not key or not keyColumnName or not dataColumnName then return false end
+
     local isKeyColumn = doesColumnExist(tableName, keyColumnName)
     if not isKeyColumn then return false end
     local isDataColumn = doesColumnExist(tableName, dataColumnName)
     if not isDataColumn then
-        connectedDB:exec("ALTER TABLE `"..tableName.."` ADD COLUMN `"..dataColumnName.."` TEXT")
-        connectedDB:exec("UPDATE `"..tableName.."` SET `"..dataColumnName.."`='"..data.."' WHERE `"..keyColumnName.."`='"..key.."'")
-    else
-        connectedDB:exec("UPDATE `"..tableName.."` SET `"..dataColumnName.."`='"..data.."' WHERE `"..keyColumnName.."`='"..key.."'")
+        connectedDB:exec("ALTER TABLE `??` ADD COLUMN `??` TEXT", tableName, dataColumnName)
     end
+    connectedDB:exec("UPDATE `??` SET `??`=? WHERE `??`=?", tableName, dataColumnName, data, keyColumnName, key)
     return true
 
 end
