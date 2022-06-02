6c22298b2df9a469ed295a60b18081cad0b913d3
Aviril <89867985+Aviril@users.noreply.github.com>
2022-06-02 12:24:34 +0300

    Update character.lua

--- module/DBify_Library/dep/module/character.lua
+++ module/DBify_Library/dep/module/character.lua
@@ -4,6 +4,7 @@
 
 local imports = {
     type = type,
+    unpack = unpack,
     addEventHandler = addEventHandler,
     dbQuery = dbQuery,
     dbPoll = dbPoll,
@@ -22,13 +23,17 @@ dbify.character = {
         keyColumn = "id"
     },
 
-    fetchAll = function(keyColumns, callback, ...)
+    fetchAll = function(...)
         if not dbify.mysql.connection.instance then return false end
-        return dbify.mysql.table.fetchContents(dbify.character.connection.table, keyColumns, callback, ...)
+        local cArgs = {dbify.parseArgs(2, ...)}
+        local keyColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
+        return dbify.mysql.table.fetchContents(dbify.character.connection.table, keyColumns, callback, imports.unpack(cArgs))
     end,
 
-    create = function(callback, ...)
+    create = function(...)
         if not dbify.mysql.connection.instance then return false end
+        local cArgs = {dbify.parseArgs(1, ...)}
+        local callback = dbify.fetchArg(_, cArgs)
         if not callback or (imports.type(callback) ~= "function") then return false end
         imports.dbQuery(function(queryHandler, arguments)
             local callbackReference = callback
@@ -37,12 +42,14 @@ dbify.character = {
             if callbackReference and (imports.type(callbackReference) == "function") then
                 callbackReference(result, arguments)
             end
-        end, {{...}}, dbify.mysql.connection.instance, "INSERT INTO `??` (`??`) VALUES(NULL)", dbify.character.connection.table, dbify.character.connection.keyColumn)
+        end, {cArgs}, dbify.mysql.connection.instance, "INSERT INTO `??` (`??`) VALUES(NULL)", dbify.character.connection.table, dbify.character.connection.keyColumn)
         return true
     end,
 
-    delete = function(characterID, callback, ...)
+    delete = function(...)
         if not dbify.mysql.connection.instance then return false end
+        local cArgs = {dbify.parseArgs(2, ...)}
+        local characterID, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
         if not characterID or (imports.type(characterID) ~= "number") then return false end
         return dbify.character.getData(characterID, {dbify.character.connection.keyColumn}, function(result, arguments)
             local callbackReference = callback
@@ -56,23 +63,27 @@ dbify.character = {
                     callbackReference(false, arguments)
                 end
             end
-        end, ...)
+        end, imports.unpack(cArgs))
     end,
 
-    setData = function(characterID, dataColumns, callback, ...)
+    setData = function(...)
         if not dbify.mysql.connection.instance then return false end
+        local cArgs = {dbify.parseArgs(3, ...)}
+        local characterID, dataColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
         if not characterID or (imports.type(characterID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return false end
         return dbify.mysql.data.set(dbify.character.connection.table, dataColumns, {
             {dbify.character.connection.keyColumn, characterID}
-        }, callback, ...)
+        }, callback, imports.unpack(cArgs))
     end,
 
-    getData = function(characterID, dataColumns, callback, ...)
+    getData = function(...)
         if not dbify.mysql.connection.instance then return false end
+        local cArgs = {dbify.parseArgs(3, ...)}
+        local characterID, dataColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
         if not characterID or (imports.type(characterID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return false end
         return dbify.mysql.data.get(dbify.character.connection.table, dataColumns, {
             {dbify.character.connection.keyColumn, characterID}
-        }, true, callback, ...)
+        }, true, callback, imports.unpack(cArgs))
     end
 }
 
