78a0b7c1bf7b1820124c8c4d3021e3dbddb9b0e1
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-12-13 17:15:27 +0300

    Update account.lua

--- module/DBify_Library/dep/module/account.lua
+++ module/DBify_Library/dep/module/account.lua
@@ -26,16 +26,16 @@ local moduleInfo = {
 dbify.createModule(moduleInfo)
 
 imports.assetify.scheduler.execOnModuleLoad(function()
-    imports.dbExec(dbify.mysql.connection.instance, "CREATE TABLE IF NOT EXISTS `??` (`??` VARCHAR(100) PRIMARY KEY)", dbify.module.account.connection.table, dbify.module.account.connection.key)
+    imports.dbExec(dbify.mysql.connection.instance, "CREATE TABLE IF NOT EXISTS `??` (`??` VARCHAR(100) PRIMARY KEY)", dbify.module[(moduleInfo.moduleName)].connection.table, dbify.module[(moduleInfo.moduleName)].connection.key)
     if not dbify.settings.syncNativeAccounts then return false end
     local playerList = imports.getElementsByType("player")
     for i = 1, #playerList, 1 do
         local playerAccount = imports.getPlayerAccount(playerList[i])
         if playerAccount and not imports.isGuestAccount(playerAccount) then
-            dbify.module.account.create(imports.getAccountName(playerAccount))
+            dbify.module[(moduleInfo.moduleName)].create(imports.getAccountName(playerAccount))
         end
     end
     imports.addEventHandler("onPlayerLogin", root, function(_, currAccount)
-        dbify.module.account.create(imports.getAccountName(currAccount))
+        dbify.module[(moduleInfo.moduleName)].create(imports.getAccountName(currAccount))
     end)
 end)
