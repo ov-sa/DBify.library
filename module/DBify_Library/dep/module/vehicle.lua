5a78c119fde757c3f70abc5128000ac0d3bff554
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-12-13 15:14:38 +0300

    Update vehicle.lua

--- module/DBify_Library/dep/module/vehicle.lua
+++ module/DBify_Library/dep/module/vehicle.lua
@@ -25,74 +25,97 @@ dbify.vehicle = {
     },
 
     fetchAll = function(...)
-        local isAsync, cArgs = dbify.parseArgs(2, ...)
-        local promise = function()
-            local keyColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
-            return dbify.mysql.table.fetchContents(dbify.vehicle.connection.table, keyColumns, callback, imports.table.unpack(cArgs))
-        end
-        if isAsync then promise(); return isAsync
-        else return promise() end
+        local cPromise, cArgs = dbify.util.parseArgs(...)
+        if not cPromise then return false end
+        local syntaxMsg = "dbify.vehicle.fetchAll(table: keyColumns)"
+        return try({
+            exec = function(self)
+                return self:await(
+                    imports.assetify.thread:createPromise(function(resolve, reject)
+                        local keyColumns = dbify.util.fetchArg(_, cArgs)
+                        resolve(dbify.mysql.table.fetchContents(dbify.vehicle.connection.table, keyColumns), cArgs)
+                    end)
+                )
+            end,
+            catch = cPromise.reject
+        })
     end,
 
     create = function(...)
-        local isAsync, cArgs = dbify.parseArgs(1, ...)
-        local promise = function()
-            if not dbify.mysql.connection.instance then return false end
-            local callback = dbify.fetchArg(_, cArgs)
-            if not callback or (imports.type(callback) ~= "function") then return false end
-            imports.dbQuery(function(queryHandler, cArgs)
-                local _, _, vehicleID = imports.dbPoll(queryHandler, 0)
-                local result = imports.tonumber((vehicleID)) or false
-                execFunction(callback, result, cArgs)
-            end, {cArgs}, dbify.mysql.connection.instance, "INSERT INTO `??` (`??`) VALUES(NULL)", dbify.vehicle.connection.table, dbify.vehicle.connection.key)
-            return true
-        end
-        if isAsync then promise(); return isAsync
-        else return promise() end
+        local cPromise, cArgs = dbify.util.parseArgs(...)
+        if not cPromise then return false end
+        local syntaxMsg = "dbify.vehicle.create()"
+        return try({
+            exec = function(self)
+                return self:await(
+                    imports.assetify.thread:createPromise(function(resolve, reject)
+                        imports.dbQuery(function(queryHandler, cArgs)
+                            local _, _, vehicleID = imports.dbPoll(queryHandler, 0)
+                            local result = imports.tonumber((vehicleID)) or false
+                            resolve(result, cArgs)
+                        end, dbify.mysql.connection.instance, "INSERT INTO `??` (`??`) VALUES(NULL)", dbify.vehicle.connection.table, dbify.vehicle.connection.key)
+                    end)
+                )
+            end,
+            catch = cPromise.reject
+        })
     end,
 
     delete = function(...)
-        local isAsync, cArgs = dbify.parseArgs(2, ...)
-        local promise = function()
-            local vehicleID, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
-            if not vehicleID or (imports.type(vehicleID) ~= "number") then return false end
-            return dbify.vehicle.getData(vehicleID, {dbify.vehicle.connection.key}, function(result, cArgs)
-                if result then
-                    result = imports.dbExec(dbify.mysql.connection.instance, "DELETE FROM `??` WHERE `??`=?", dbify.vehicle.connection.table, dbify.vehicle.connection.key, vehicleID)
-                    execFunction(callback, result, cArgs)
-                else
-                    execFunction(callback, false, cArgs)
-                end
-            end, imports.table.unpack(cArgs))
-        end
-        if isAsync then promise(); return isAsync
-        else return promise() end
+        local cPromise, cArgs = dbify.util.parseArgs(...)
+        if not cPromise then return false end
+        local syntaxMsg = "dbify.vehicle.delete(int: vehicleID)"
+        return try({
+            exec = function(self)
+                return self:await(
+                    imports.assetify.thread:createPromise(function(resolve, reject)
+                        if not vehicleID or (imports.type(vehicleID) ~= "number") then return dbify.util.throwError(reject, syntaxMsg) end
+                        local isExisting = dbify.vehicle.getData(vehicleID, {dbify.vehicle.connection.key})
+                        if not isExisting then return resolve(isExisting, cArgs) end
+                        resolve(imports.dbExec(dbify.mysql.connection.instance, "DELETE FROM `??` WHERE `??`=?", dbify.vehicle.connection.table, dbify.vehicle.connection.key, vehicleID), cArgs)
+                    end)
+                )
+            end,
+            catch = cPromise.reject
+        })
     end,
 
     setData = function(...)
-        local isAsync, cArgs = dbify.parseArgs(3, ...)
-        local promise = function()
-            local vehicleID, dataColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
-            if not vehicleID or (imports.type(vehicleID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return false end
-            return dbify.mysql.data.set(dbify.vehicle.connection.table, dataColumns, {
-                {dbify.vehicle.connection.key, vehicleID}
-            }, callback, imports.table.unpack(cArgs))
-        end
-        if isAsync then promise(); return isAsync
-        else return promise() end
+        local cPromise, cArgs = dbify.util.parseArgs(...)
+        if not cPromise then return false end
+        local syntaxMsg = "dbify.vehicle.setData(int: vehicleID, table: dataColumns)"
+        return try({
+            exec = function(self)
+                return self:await(
+                    imports.assetify.thread:createPromise(function(resolve, reject)
+                        local vehicleID, dataColumns = dbify.util.fetchArg(_, cArgs), dbify.util.fetchArg(_, cArgs)
+                        if not vehicleID or (imports.type(vehicleID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return dbify.util.throwError(reject, syntaxMsg) end
+                        local isExisting = dbify.vehicle.getData(vehicleID, {dbify.vehicle.connection.key})
+                        if not isExisting then return resolve(isExisting, cArgs) end
+                        resolve(dbify.mysql.data.set(dbify.vehicle.connection.table, dataColumns, { {dbify.vehicle.connection.key, vehicleID} }), cArgs)                        
+                    end)
+                )
+            end,
+            catch = cPromise.reject
+        })
     end,
 
     getData = function(...)
-        local isAsync, cArgs = dbify.parseArgs(3, ...)
-        local promise = function()
-            local vehicleID, dataColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
-            if not vehicleID or (imports.type(vehicleID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return false end
-            return dbify.mysql.data.get(dbify.vehicle.connection.table, dataColumns, {
-                {dbify.vehicle.connection.key, vehicleID}
-            }, true, callback, imports.table.unpack(cArgs))
-        end
-        if isAsync then promise(); return isAsync
-        else return promise() end
+        local cPromise, cArgs = dbify.util.parseArgs(...)
+        if not cPromise then return false end
+        local syntaxMsg = "dbify.vehicle.getData(int: vehicleID, table: dataColumns)"
+        return try({
+            exec = function(self)
+                return self:await(
+                    imports.assetify.thread:createPromise(function(resolve, reject)
+                        local vehicleID, dataColumns = dbify.util.fetchArg(_, cArgs), dbify.util.fetchArg(_, cArgs)
+                        if not vehicleID or (imports.type(vehicleID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return dbify.util.throwError(reject, syntaxMsg) end
+                        resolve(dbify.mysql.data.get(dbify.vehicle.connection.table, dataColumns, { {dbify.vehicle.connection.key, vehicleID} }, true), cArgs)                        
+                    end)
+                )
+            end,
+            catch = cPromise.reject
+        })
     end
 }
 
