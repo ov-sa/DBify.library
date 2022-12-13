98e12abe7e1ac92e360764dc44147b4a2b87bfd4
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-12-13 17:18:55 +0300

    Updated

--- module/DBify_Library/dep/module/account.lua
+++ module/DBify_Library/dep/module/account.lua
@@ -28,9 +28,9 @@ dbify.createModule(moduleInfo)
 imports.assetify.scheduler.execOnModuleLoad(function()
     imports.dbExec(dbify.mysql.connection.instance, "CREATE TABLE IF NOT EXISTS `??` (`??` VARCHAR(100) PRIMARY KEY)", dbify.module[(moduleInfo.moduleName)].connection.table, dbify.module[(moduleInfo.moduleName)].connection.key)
     if not dbify.settings.syncNativeAccounts then return false end
-    local playerList = imports.getElementsByType("player")
-    for i = 1, #playerList, 1 do
-        local playerAccount = imports.getPlayerAccount(playerList[i])
+    local serverPlayers = imports.getElementsByType("player")
+    for i = 1, #serverPlayers, 1 do
+        local playerAccount = imports.getPlayerAccount(serverPlayers[i])
         if playerAccount and not imports.isGuestAccount(playerAccount) then
             dbify.module[(moduleInfo.moduleName)].create(imports.getAccountName(playerAccount))
         end
--- module/DBify_Library/dep/module/serial.lua
+++ module/DBify_Library/dep/module/serial.lua
@@ -26,9 +26,9 @@ dbify.createModule(moduleInfo)
 imports.assetify.scheduler.execOnModuleLoad(function()
     imports.dbExec(dbify.mysql.connection.instance, "CREATE TABLE IF NOT EXISTS `??` (`??` VARCHAR(100) PRIMARY KEY)", dbify.module[(moduleInfo.moduleName)].connection.table, dbify.module[(moduleInfo.moduleName)].connection.key)
     if not dbify.settings.syncNativeSerials then return false end
-    local playerList = imports.getElementsByType("player")
-    for i = 1, #playerList, 1 do
-        local playerSerial = imports.getPlayerSerial(playerList[i])
+    local serverPlayers = imports.getElementsByType("player")
+    for i = 1, #serverPlayers, 1 do
+        local playerSerial = imports.getPlayerSerial(serverPlayers[i])
         dbify.module[(moduleInfo.moduleName)].create(playerSerial)
     end
     imports.addEventHandler("onPlayerJoin", root, function()
